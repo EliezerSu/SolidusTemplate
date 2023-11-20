0937973ae8b8fc44cae17ce1f089b2ef9075ef9e
Eliezer suarez <eliezer.suarez@iguanadigital.com.ec>
2023-11-20 10:15:18 -0500

    Añado animacion de collapse

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,7 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('collapsed');
+    
+    const sidebarToggleWrapper = document.querySelector('.sidebar-toggle-wrapper');
+  sidebarToggleWrapper.classList.toggle('collapsed');
 }
